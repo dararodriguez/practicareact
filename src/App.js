7283350ff8bccda1decd3110141a40f429c88b61
diego <dara931225@hotmail.com>
2021-03-17 21:59:20 -0500

    adicone apellido

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Diego Rodriguez</code>
+          <code>Diego Rodriguez Arciniegas</code>
         </p>
         <a
           className="App-link"
