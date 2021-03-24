090d406da8f451ca5332141810c7ffad9fd38853
diego <dara931225@hotmail.com>
2021-03-24 18:29:34 -0500

    subida interfaz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.backgroundColor='#00000'
+  },
+}));
+
+export default function FullWidthGrid() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Diego Rodriguez Arciniegas</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Container maxWidth="sm">
+      <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '50vh' }}>
+
+      <Grid container spacing={3}>
+      <Grid item xs={12}>
+          <Paper className={classes.paper}>0</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}><Button variant="contained" color="primary">
+  1
+</Button></Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}><Button variant="contained" color="primary">
+  2
+</Button></Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}><Button variant="contained" color="primary">
+  3
+</Button></Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}><Button variant="contained" color="secondary">
+  +
+</Button></Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}><Button variant="contained" color="primary">
+  4
+</Button></Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}><Button variant="contained" color="primary">
+  5
+</Button></Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}><Button variant="contained" color="primary">
+  6
+</Button></Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}><Button variant="contained" color="secondary">
+  -
+</Button></Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}><Button variant="contained" color="primary">
+  7
+</Button></Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}><Button variant="contained" color="primary">
+  8
+</Button></Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}><Button variant="contained" color="primary">
+  9
+</Button></Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}><Button variant="contained" color="secondary">
+  *
+</Button></Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}><Button variant="contained" color="primary">
+  0
+</Button></Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}><Button variant="contained" color="primary">
+  ,
+</Button></Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}><Button variant="contained" color="secondary">
+  =
+</Button></Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}><Button variant="contained" color="secondary">
+  /
+</Button></Paper>
+        </Grid>
+      </Grid>
+      </Typography>
+      </Container>
     </div>
   );
 }
-
-export default App;
