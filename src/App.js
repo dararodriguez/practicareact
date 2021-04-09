8e995368ed99f5a0ca8a65040da162cec6d8beeb
diego <dara931225@hotmail.com>
2021-04-08 23:20:08 -0500

    se elimina linea no usada

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
